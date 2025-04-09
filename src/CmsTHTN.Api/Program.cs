6c9d8d4bab45e3e68aeead3ae3c2a749d398a845
Nguyễn Mạnh <manhnt8803@gmail.com>
2025-04-09 17:11:34 +0700

    complete authen & author API JWT Token

--- src/CmsTHTN.Api/Program.cs
+++ src/CmsTHTN.Api/Program.cs
@@ -18,6 +18,15 @@ var culture = CultureInfo.InvariantCulture;
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
 var connectionString = configuration.GetConnectionString("DefaultConnection");
+var CmsTHTNCorsPolicy = "CmsTHTNCorsPolicy";
+
+builder.Services.AddCors(o => o.AddPolicy(CmsTHTNCorsPolicy, builder =>
+{
+    builder.AllowAnyMethod()
+        .AllowAnyHeader()
+        .WithOrigins(configuration["AllowedOrigins"])
+        .AllowCredentials();
+}));
 
 //Config DB Context and ASP.NET Core Identity
 builder.Services.AddDbContext<CmsTHTNContext>(options => options.UseSqlServer(connectionString));
@@ -103,6 +112,8 @@ if (app.Environment.IsDevelopment())
         c.DisplayRequestDuration();
     });
 }
+
+app.UseCors(CmsTHTNCorsPolicy);
 
 app.UseHttpsRedirection();
 
