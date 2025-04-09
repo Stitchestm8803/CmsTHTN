4fb891d4dbf4730b4774f10c0e6cd95d8e38c3ed
Nguyễn Mạnh <manhnt8803@gmail.com>
2025-04-09 18:11:06 +0700

    complete login & logout

--- CmsTHTN-admin-ui/src/app/views/auth/login/login.component.ts
+++ CmsTHTN-admin-ui/src/app/views/auth/login/login.component.ts
@@ -1,22 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AdminApiAuthApiClient, AuthenticatedResult, LoginRequest } from 'src/app/api/admin-api.service.generated'
-import { AlertService } from 'src/app/shared/services/alert.service'
+import { AdminApiAuthApiClient, AuthenticatedResult, LoginRequest } from './../../../api/admin-api.service.generated'
+import { AlertService } from './../../../shared/services/alert.service'
+import { UrlConstants } from '../../../shared/constants/url.constants';
+import { TokenStorageService } from '../../../shared/services/token-storage.service';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnDestroy{
   loginForm: FormGroup;
+  private ngUnsubcribe = new Subject<void>();
+  loading = false;
 
   constructor(
     private fb: FormBuilder, 
     private authApiClient: AdminApiAuthApiClient,
     private alertService: AlertService,
-    private router: Router
+    private router: Router,
+    private tokenService: TokenStorageService
   ) { 
     this.loginForm = this.fb.group({
       userName: new FormControl('', Validators.required),
@@ -24,21 +30,32 @@ export class LoginComponent {
     });
   }
 
+  ngOnDestroy(): void {
+    this.ngUnsubcribe.next();
+    this.ngUnsubcribe.complete();
+  }
+
   login(){
+    this.loading = true;
     var request: LoginRequest = new LoginRequest({
       userName: this.loginForm.controls['userName'].value,
       password: this.loginForm.controls['password'].value,
     });
-    this.authApiClient.login(request).subscribe({
+    this.authApiClient.login(request)
+    .pipe(takeUntil(this.ngUnsubcribe))
+    .subscribe({
       next: (res: AuthenticatedResult) => {
         //Save token and refresh token to localstorage
-
+        this.tokenService.saveToken(res.token ?? '');   
+        this.tokenService.saveRefreshToken(res.refreshToken ?? '');
+        this.tokenService.saveUser(res);
         //Redirect to dashboard
-        this.router.navigate(['/dashboard']);
+        this.router.navigate([UrlConstants.HOME]);
       },
       error: (error: any) => {
         console.log(error);
-        this.alertService.showError('Login invalid');
+        this.alertService.showError('Đăng nhập thất bại.');
+        this.loading = false;
       },
     });
   }
