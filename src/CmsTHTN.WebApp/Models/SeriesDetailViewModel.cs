4b3e0e17d10782ea10b838056ac35b47db3c17c4
Nguyễn Mạnh <manhnt8803@gmail.com>
2025-04-17 16:46:50 +0700

    show series for visitors

---
+++ src/CmsTHTN.WebApp/Models/SeriesDetailViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CmsTHTN.Core.Models.Content;
+using CmsTHTN.Core.Models;
+
+namespace CmsTHTN.WebApp.Models
+{
+    public class SeriesDetailViewModel
+    {
+        public SeriesDto Series { get; set; }
+
+        public PagedResult<PostInListDto> Posts { get; set; }
+    }
+}
