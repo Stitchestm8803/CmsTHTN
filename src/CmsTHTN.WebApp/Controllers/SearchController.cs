c551bb7716c247a5547844d17280c7e16c5fecd4
Nguyễn Mạnh <manhnt8803@gmail.com>
2025-04-27 13:58:04 +0700

    update search for visitors

---
+++ src/CmsTHTN.WebApp/Controllers/SearchController.cs
@@ -0,0 +1,48 @@
+﻿using CmsTHTN.Core.Domain.Content;
+using CmsTHTN.Core.Models.Content;
+using CmsTHTN.Core.Models;
+using CmsTHTN.Core.SeedWorks;
+using CmsTHTN.Data;
+using CmsTHTN.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+
+namespace CmsTHTN.WebApp.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly CmsTHTNContext _context;
+        private readonly IMapper _mapper;
+
+        public SearchController(CmsTHTNContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [Route("posts/search")]
+        public async Task<IActionResult> Index([FromQuery] string keyword, [FromQuery] int page = 1, int pageSize = 5)
+        {
+            var query = _context.Posts.Where(post => post.Status == PostStatus.Published);
+
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                query = query.Where(post => post.Name.Contains(keyword) || post.Tags.Contains(keyword) || post.CategoryName.Contains(keyword));
+            }
+
+            query = query.OrderByDescending(post => post.ViewCount);
+
+            var totalItems = await query.CountAsync();
+
+            var pagedPosts = await query.Skip((page - 1) * pageSize).Take(pageSize).ToListAsync();
+
+            var mappedPosts = _mapper.Map<List<PostInListDto>>(pagedPosts);
+
+            return View(new AllPostViewModel
+            {
+                Posts = new PagedResult<PostInListDto> { Results = mappedPosts, RowCount = totalItems, CurrentPage = page, PageSize = pageSize }
+            });
+        }
+    }
+}
